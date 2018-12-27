15aee80489eaa3f4c8b8e7abadda14ffa1cb7459
Serj <sgtserj94@gmail.com>
2018-12-28 01:04:35 +0900

    Лабораторная №3

--- src/MongoGo.cs
+++ src/MongoGo.cs
@@ -12,10 +12,12 @@ namespace src
         }
         public void start()
         {
-            // // or use a connection string
             var client = new MongoClient("mongodb://localhost:27017");
             var database = client.GetDatabase("test");
             var collection = database.GetCollection<BsonDocument>("person");
+
+            //Задание №1
+
             var document = new BsonDocument
                 {
                     { "name", "Кононов Петр" },
@@ -27,20 +29,72 @@ namespace src
                             "Футбол","Покер"
                         }}
                 };
-            // collection.InsertOne(document);
+            collection.InsertOne(document);
 
-            // var document2 = collection.Find(new BsonDocument()).Sort("{name: 1}").ToList();
-            // foreach (var p in document2)
-            // {
-            //      Console.WriteLine(p.ToString());
-            // }
+            //Задание №2
 
-            var document3 = collection.Find(Builders<BsonDocument>.Filter.SizeGt("hobby", 0)).Sort("{name: 1}").ToList();
-            Console.WriteLine("lol");
+            var document2 = collection.Find(new BsonDocument()).Sort("{name: 1}").ToList();
+            foreach (var p in document2)
+            {
+                 Console.WriteLine(p.ToString());
+            }
+
+            //Задание №3
+
+            var document3 = collection.Find(new BsonDocument("hobby", new BsonDocument("$exists", 1))).Sort("{name: 1}").ToList();
             foreach (var p in document3)
             {
                  Console.WriteLine(p.ToString());
             }
+            
+            //Задание №4
+
+            var document4 = collection.Find(Builders<BsonDocument>.Filter.Gte("name", "К")).ToList();
+            foreach (var p in document4)
+            {
+                 Console.WriteLine(p.ToString());
+            }
+            
+            //Задание №5
+            
+            var document5 = collection.Find(
+                new BsonDocument(
+                    "$and", new BsonArray{
+                        new BsonDocument(
+                        "$and", new BsonArray{
+                            new BsonDocument("birth", new BsonDocument("$lte", new DateTime(1992,12,27,0,00,0,0))),
+                            new BsonDocument("birth", new BsonDocument("$gte", new DateTime(1968,12,27,0,00,0,0)))
+                        }
+                        ),
+                        new BsonDocument("hobby", new BsonDocument("$exists", 1))
+                    }
+                )
+            ).Sort("{birth: 1}").ToList();
+            foreach (var p in document5)
+            {
+                 Console.WriteLine(p.ToString());
+            }
+            
+            //Задание №6
+            
+            var document6 = collection.UpdateOne(new BsonDocument("name", "Винокуров Виктор"), new BsonDocument("$set", new BsonDocument("prof", "Программист")));
+            
+            //Задание №7
+            
+            var document7 = collection.Find(
+                new BsonDocument(
+                    "hobby", new BsonDocument (
+                        "$elemMatch", new BsonDocument (
+                            "$in", new BsonArray().Add("Музыка")
+                        )
+                    )
+                )
+            ).Sort("{name: 1}").ToList();
+            foreach (var p in document7)
+            {
+                 Console.WriteLine(p.ToString());
+            }
+
         }
     }
 }
